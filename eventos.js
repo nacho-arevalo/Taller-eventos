264a3141f8f8de4c6a1db081cda19e1f4f7d17e2
nacho arevalo <elnachoamsn@gmail.com>
2023-10-01 22:03:20 -0300

    Terminando

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Obtenemos una referencia al div por su ID
+const miDiv = document.getElementById("miDiv");
+
+// Agregamos un manejador de eventos para el evento "click"
+miDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
